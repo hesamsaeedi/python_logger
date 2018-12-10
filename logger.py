b3fcd27df76bd63351045bcabfe71f30cd92593a
hesamsaeedi <40423984+hesamsaeedi@users.noreply.github.com>
2018-12-10 11:10:53 +0330

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,18 @@
+import logging
+import time
+
+logger = logging.getLogger('monitor_log')
+logger.setLevel(logging.INFO)
+
+fh = logging.FileHandler('monitor.log')
+fh.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
+
+logger.info('My message in logger')
+
+time.sleep(1.5)
+logger.info('My 2nd message in logger')
